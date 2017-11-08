9a2b2b723c607baa9a879eca5bb3d5d04db29295
Enrico Guiraud <enrico.guiraud@cern.ch>
2017-11-09 00:26:55 +0100

    Make TBankDecoder's methods const when appropriate
    Also fix missing dtor body.

--- src/TBankDecoder.hxx
+++ src/TBankDecoder.hxx
@@ -9,17 +9,19 @@
 /// TMDFDataSource uses concrete TBankDecoder implementations to read banks.
 class TBankDecoder {
 public:
-   virtual ~TBankDecoder();
+   virtual ~TBankDecoder() {}
    /// Retrieve bank name. Corresponds to the column name used in TDataFrame to refer to this bank.
-   virtual std::string GetName() = 0;
+   virtual std::string GetName() const = 0;
+   /// Retrieve the type name of this bank as a string.
+   virtual std::string GetTypeName() const = 0;
    /// Retrieve bank ID.
-   virtual int GetID() = 0;
+   virtual int GetID() const = 0;
    /// Take a bank, return its decoding
-   virtual void *Decode(const char *bank) = 0;
+   virtual void *Decode(const char *bank) const = 0;
    /// Allocate N objects of the correct type, after checking that we expect the right type
-   virtual std::vector<void *> Allocate(unsigned int N, const std::type_info &expectedType) = 0;
+   virtual std::vector<void *> Allocate(unsigned int N, const std::type_info &expectedType) const = 0;
    /// Deallocate objects by calling the destructor for the appropriate type
-   virtual void Deallocate(const std::vector<void *> &objs) = 0;
+   virtual void Deallocate(const std::vector<void *> &objs) const = 0;
 };
 
 #endif
