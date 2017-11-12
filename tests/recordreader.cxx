bc9c74f081bc2dcac6dc0142e948299e3932fb86
Enrico Guiraud <enrico.guiraud@cern.ch>
2017-11-12 17:43:02 +0100

    Add gtest for bank traversing

--- tests/recordreader.cxx
+++ tests/recordreader.cxx
@@ -1,18 +1,38 @@
 #include <TRecordReader.hxx>
-#include <TSystem.h>
 #include <gtest/gtest.h>
 #include <iostream>
+#include <iomanip>
+
+const auto fname = "small.raw";
+
+struct with_width {
+   int w;
+   with_width(int w) : w(w) {}
+};
+auto operator<<(std::ostream &os, const with_width &w) -> decltype(os << std::setw(0) << std::left)
+{
+   return os << std::setw(w.w) << std::left;
+}
 
 TEST(RecordReader, TraverseRecords)
 {
-   const auto fname = "small.raw";
-   const auto fileExists = !gSystem->AccessPathName(fname); // weird return value convention
-   ASSERT_TRUE(fileExists) << "Could not run test: test file not found.";
-
    TRecordReader r(fname);
-   for (auto counter = 0u; r.NextRecord(); ++counter) {
-      std::cout << counter << "\t current position: " << r.GetRecordPosition()
-                << "\t record size: " << r.GetRecordSize() << '\n';
+   for (auto recordn = 0u; r.NextRecord(); ++recordn)
+      std::cout << "record " << with_width(7) << recordn << " pos: " << with_width(10) << r.GetRecordPosition()
+                << " size: " << with_width(7) << r.GetRecordSize() << '\n';
+}
+
+TEST(RecordReader, TraverseBanks)
+{
+   TRecordReader r(fname);
+   for (auto recordn = 0u; r.NextRecord(); ++recordn) {
+      std::cout << "record " << with_width(7) << recordn << '\n';
+      for (auto bankn = 0u; r.NextBank(); ++bankn) {
+         auto bankHeader = r.GetBankHeader();
+         std::cout << "    bank " << with_width(7) << bankn << " pos: " << with_width(10) << r.GetBankPosition()
+                   << " size: " << with_width(7) << bankHeader.size << " type: " << with_width(7) << bankHeader.type
+                   << " version: " << with_width(7) << bankHeader.version << '\n';
+      }
    }
 }
 
