c5e956b7ab9c3b6dc33e6997b223bc1ca5296f2f
Enrico Guiraud <enrico.guiraud@cern.ch>
2017-11-12 01:17:29 +0100

    WIP nextbank, readbankheader

--- src/TRecordReader.hxx
+++ src/TRecordReader.hxx
@@ -10,6 +10,7 @@ public:
 private:
    std::filebuf fFileBuf;         ///< stream buffer
    pos_type fCurrentRecord = 0;   ///< position of current record in file
+   pos_type fCurrentBank = 0;     ///< position of current bank in file
    unsigned int fRecordSize = 0u; ///< size of current record
    /// Get size of current record, or 0 if record is corrupted
    unsigned int EvalRecordSize();
@@ -22,6 +23,10 @@ public:
    TRecordReader(const std::string &fname);
    /// Move this RecordReader to the next valid record in the file. Return false if there are no further records.
    bool NextRecord();
+   /// Move this RecordReader to the next bank in the record. Return false if there are no further banks.
+   bool NextBank();
+   /// Extract information from the bank header
+   BankHeader ReadBankHeader();
    /// Return position of the current record
    pos_type GetRecordPosition() { return fCurrentRecord; }
    unsigned int GetRecordSize() { return fRecordSize; }
