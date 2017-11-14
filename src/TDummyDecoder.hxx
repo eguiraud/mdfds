63cecefaacc9179f793b69aa3a63dcead0c91191
Enrico Guiraud <enrico.guiraud@cern.ch>
2017-11-14 15:57:04 +0100

    Add gtest for TMDFDataSource::SetEntry

--- src/TDummyDecoder.hxx
+++ src/TDummyDecoder.hxx
@@ -8,7 +8,7 @@ public:
    std::string GetName() const final { return "dummy"; }
    std::string GetTypeName() const final { return "int"; }
    const std::type_info &GetTypeInfo() const { return typeid(int); }
-   int GetID() const final { return 123456; }
+   int GetID() const final { return 0; } // hi-jacking a real bank ID for our purposes
    void Decode(const char *, void *destination) const final { *static_cast<int *>(destination) = 42; }
    void *Allocate() const final;
    void Deallocate(void *obj) const final;
--- tests/mdfds_test.cxx
+++ tests/mdfds_test.cxx
@@ -5,6 +5,9 @@
 
 #include <gtest/gtest.h>
 
+#include <random>
+#include <vector>
+
 TEST(MDFDS, DummyDecoder)
 {
    // file does not have to exist
@@ -33,9 +36,33 @@ TEST(MDFDS, GetColumnReadersMT)
    TMDFDataSource<TDummyDecoder> ds({"small.raw"});
    ds.SetNSlots(4u);
    const std::vector<int **> readers = ds.GetColumnReaders<int>("dummy");
+   EXPECT_EQ(readers.size(), 4u);
    for (auto &r : readers) {
       EXPECT_NE(r, nullptr);
       EXPECT_EQ(**r, 42); // check we can dereference twice without crashing
+   }
+}
+
+TEST(MDFDS, SetEntry)
+{
+   TMDFDataSource<TDummyDecoder> ds({"small.raw"});
+   ds.SetNSlots(2);
+   const auto readers = ds.GetColumnReaders<int>("dummy");
+
+   auto ranges = ds.GetEntryRanges();
+   std::random_device rd;
+   std::mt19937 g(rd());
+   std::shuffle(ranges.begin(), ranges.end(), g);
+
+   while (!ranges.empty()) {
+      for (const auto &r : ranges) {
+         for (auto entry = r.first; entry < r.second; ++entry) {
+            ds.SetEntry(0u, entry);
+            for (auto &v : readers)
+               EXPECT_EQ(**v, 42);
+         }
+      }
+      ranges = ds.GetEntryRanges();
    }
 }
 
