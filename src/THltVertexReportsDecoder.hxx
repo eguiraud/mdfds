1051a274e7743b241065f3fb85e65dcef339e85e
Enrico Guiraud <enrico.guiraud@cern.ch>
2017-11-17 17:52:02 +0100

    Fix THltVertexReportsDecoder::GetTypeName

--- src/THltVertexReportsDecoder.hxx
+++ src/THltVertexReportsDecoder.hxx
@@ -11,7 +11,7 @@ class THltVertexReportsDecoder final : public TBankDecoder {
 public:
    ~THltVertexReportsDecoder() {}
    std::string GetName() const final { return "HltVertexReports"; };     // TODO: shouldn't this be a const ref?
-   std::string GetTypeName() const final { return "HltVertexReports"; }; // TODO: shouldn't this be a const ref?
+   std::string GetTypeName() const final { return "THltVertexReports"; }; // TODO: shouldn't this be a const ref?
    const std::type_info &GetTypeInfo() const final;
    EBankType GetID() const;
    void Decode(std::array_view<char> bank, void *destination) const final;
